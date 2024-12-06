aafdefd34fb4a5562c942deeb43ac24cfa05134f
SyoBoN <syobon@syobon.net>
2024-12-06 21:57:08 +0900

    tweak

--- src/service/ccid/ccid.v
+++ src/service/ccid/ccid.v
@@ -1,10 +1,13 @@
 module ccid
 
+import crypto.sha256
 import ismyhc.vbech32
+import v_crypto.ripemd160
 import service.key
 
 pub fn pubkey_to_addr(pubkey []u8, hrp string) !string {
-	return vbech32.encode_from_base256(hrp, pubkey)!
+	addr := get_pubkey_address(pubkey)
+	return vbech32.encode_from_base256(hrp, addr)!
 }
 
 pub fn pubkey_to_ccid(pubkey []u8) !string {
@@ -25,4 +28,10 @@ pub fn privkey_to_csid(privkey_hex string) !string {
 	pubkey := key.privkey_to_pubkey(privkey_hex)!
 
 	return pubkey_to_csid(pubkey)
+}
+
+pub fn get_pubkey_address(pubkey []u8) []u8 {
+	hash := sha256.sum256(pubkey)
+	digest := ripemd160.new()
+	return digest.sum(hash)
 }

ae3f0702cc6533476f91ff56ee5ccc2ef78507f4
SyoBoN <syobon@syobon.net>
2024-12-07 11:12:06 +0900

    commit now recieves string instead of Document

--- src/server/web.v
+++ src/server/web.v
@@ -1,10 +1,8 @@
 module server
 
 import encoding.base64
-import json
 import log
 import veb
-import model
 import service.store
 
 @['/web/register']
@@ -12,18 +10,14 @@ pub fn (app &App) register(mut ctx Context) veb.Result {
 	registration_encoded := ctx.query['registration'] or {
 		return ctx.request_error('Please provide a registration query parameter')
 	}
-	registration_json := base64.decode_str(registration_encoded.replace_each(['-', '+', '_', '/']))
-	registration := json.decode(model.AffiliationDocument, registration_json) or {
-		return ctx.request_error('Invalid json')
-	}
+	registration := base64.decode_str(registration_encoded.replace_each(['-', '+', '_', '/']))
 	signature := ctx.query['signature'] or {
 		return ctx.request_error('Please provide a signature')
 	}
 
 	match app.data.metadata.registration {
 		.open {
-			registered_ccid := store.commit(.execute, registration, registration_json,
-				signature) or {
+			registered_ccid := store.commit(.execute, registration, signature) or {
 				log.error('Failed to register a new user: ${err}')
 				return ctx.server_error('Failed to register a new user.')
 			}
--- src/service/store/store.v
+++ src/service/store/store.v
@@ -1,6 +1,7 @@
 module store
 
 import encoding.hex
+import json
 // import conf
 import model
 import service.entity
@@ -12,7 +13,8 @@ pub enum CommitMode {
 	local_only_execute
 }
 
-pub fn commit(mode CommitMode, document model.Document, document_raw string, sig string) !string {
+pub fn commit(mode CommitMode, document_raw string, sig string) !string {
+	document := json.decode(model.DocumentBase, document_raw)!
 	if document.key_id == '' {
 		signature_bytes := hex.decode(sig)!
 		signature.verify(document_raw.bytes(), signature_bytes, document.signer)!
@@ -37,9 +39,10 @@ pub fn commit(mode CommitMode, document model.Document, document_raw string, sig
 		signature.verify(document_raw.bytes(), signature_bytes, document.key_id)!
 		*/
 	}
-	match document {
-		model.AffiliationDocument {
-			return entity.affiliation(document, sig)
+	match document.type {
+		.affiliation {
+			affiliation_document := json.decode(model.AffiliationDocument, document_raw)!
+			return entity.affiliation(affiliation_document, sig)
 		}
 		else {
 			return error('Not implemented yet')

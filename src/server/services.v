cd46f6962e57f336e5da2189c7fa876377bcaaaa
SyoBoN <syobon@syobon.net>
2024-12-07 20:04:15 +0900

    fix json

--- src/server/services.v
+++ src/server/services.v
@@ -5,5 +5,5 @@ import veb
 @['/services']
 pub fn (app &App) services(mut ctx Context) veb.Result {
 	access_log(ctx)
-	return ctx.send_response_to_client('application/json', '{"net.concrnt.api":{"path":"/api/v1"},"net.concrnt.webui":{"path":"/web"}')
+	return ctx.send_response_to_client('application/json', '{"net.concrnt.api":{"path":"/api/v1"},"net.concrnt.webui":{"path":"/web"}}')
 }

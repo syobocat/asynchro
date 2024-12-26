cc47ddcd42e1811ef0a857cc204453b14bb42e32
SyoBoN <syobon@syobon.net>
2024-12-27 00:04:17 +0900

    fix

--- src/server/timeline.v
+++ src/server/timeline.v
@@ -8,6 +8,14 @@ import service.timeline
 @['/api/v1/timeline/:id']
 pub fn (app &App) timeline(mut ctx Context, id string) veb.Result {
 	tlid := timeline.parse_tlid(id) or { return ctx.request_error('Invalid TimelineID') }
+	if semantic_id := tlid.semantic_id {
+		user_id := tlid.user_id or { return ctx.request_error('Invalid TimelineID') }
+		res := database.resolve_semanticid(semantic_id, user_id) or {
+			log.error('Something happend when lookup semanticID: ${err}')
+			return ctx.return_error(.internal_server_error, err.msg(), none)
+		}
+		_ := res.result or { return ctx.return_error(.not_found, 'User not found', none) }
+	}
 	normalized_wrapped := tlid.normalized() or { return ctx.request_error('Invalid TimelineID') }
 	if normalized_wrapped !is timeline.NormalizedPureTimelineID {
 		return ctx.request_error('Invalid TimelineID')
